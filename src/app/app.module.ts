daa65f9fd50198c695d3a6cb50880f6a504a65d3
Matias Olivieri <matiasolivieri34@gmail.com>
2023-01-26 22:32:09 -0300

    4 | Reactive Forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { StudentsComponent } from './components/students/students.component';
 import { DirectivasComponent } from './components/directivas/directivas.component';
 import { DirectivaPersonalizadaDirective } from './directives/directiva-personalizada.directive';
+import { FormularioReactivoComponent } from './components/formulario-reactivo/formulario-reactivo.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,10 +17,12 @@ import { DirectivaPersonalizadaDirective } from './directives/directiva-personal
     ToolbarComponent,
     StudentsComponent,
     DirectivasComponent,
-    DirectivaPersonalizadaDirective
+    DirectivaPersonalizadaDirective,
+    FormularioReactivoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
